0319d96484487ea37164eac3048eee8f4fbb2d06
Blaine Love <blainelove94@gmail.com>
2021-08-10 16:08:57 -0400

    player

--- src/Info.js
+++ src/Info.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './css/styles.css'
+import ReactPlayer from 'react-player/youtube'
 
 const info = () => {
   return (
@@ -26,7 +27,7 @@ const info = () => {
        
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top">Learn about Blaine Love</a>
+                <a class="navbar-brand" href="#page-top">Blaine Love</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
@@ -48,7 +49,7 @@ const info = () => {
                     <div class="text-center">
                         <h1 class="mx-auto my-0 text-uppercase">Blaine Love</h1>
                         
-                        <a class="btn btn-primary" href="#about">Get Started</a>
+                        <a class="btn btn-primary" href="#about">About Me</a>
                     </div>
                 </div>
             </div>
@@ -67,27 +68,17 @@ const info = () => {
                 <img class="img-fluid" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8UWaDlb9WpmuL6HXRTLu6tISiqrj57soY6g&usqp=CAU" alt="..." />
             </div>
         </section>
-       
+        <h1>Projects</h1>
         <section class="projects-section bg-light" id="projects">
-            <div class="container px-4 px-lg-5">
-               
-                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/bg-masthead.jpg" alt="..." /></div>
-                    <div class="col-xl-4 col-lg-5">
-                        <div class="featured-text text-center text-lg-left">
-                            <h4>Shoreline</h4>
-                            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
-                        </div>
-                    </div>
-                </div>
-                
+          
+            <div>
                 <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-01.jpg" alt="..." /></div>
+                    <div class="col-lg-6"><ReactPlayer url='https://www.youtube.com/watch?v=kHjaehH-EtA'  /></div>
                     <div class="col-lg-6">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-left">
-                                    <h4 class="text-white">Misty</h4>
+                                    <h4 class="text-white">Hobby Lobby</h4>
                                     <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
                                     <hr class="d-none d-lg-block mb-0 ms-0" />
                                 </div>
@@ -97,14 +88,28 @@ const info = () => {
                 </div>
                
                 <div class="row gx-0 justify-content-center">
-                    <div class="col-lg-6"><img class="img-fluid" src="assets/img/demo-image-02.jpg" alt="..." /></div>
+                    <div class="col-lg-6"><ReactPlayer url='https://www.youtube.com/watch?v=EBIsZlV1jHk'class="img-fluid"  alt="..." /></div>
                     <div class="col-lg-6 order-lg-first">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
                                 <div class="project-text w-100 my-auto text-center text-lg-right">
-                                    <h4 class="text-white">Mountains</h4>
+                                    <h4 class="text-white">Sports</h4>
                                     <p class="mb-0 text-white-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
                                     <hr class="d-none d-lg-block mb-0 me-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6" ><ReactPlayer url='https://www.youtube.com/watch?v=kHjaehH-EtA'class="img-fluid"  alt="..." /></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">How You Feelin</h4>
+                                    <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                                    <hr class="d-none d-lg-block mb-0 ms-0" />
                                 </div>
                             </div>
                         </div>
